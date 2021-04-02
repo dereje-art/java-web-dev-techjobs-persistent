e13acc05dcd94bd73b717aacabc9af9c55467f34
dereje <djdereje1@gmail.com>
2021-04-01 20:06:45 -0500

    second commit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
+CREATE TABLE job (
+    id int AUTO_INCREMENT primary key,
+    name varchar(100),
+    skill varchar(255),
+    employer_id int,
+    job_id
+);
 
 ## Part 2: Test it with SQL
+SELECT * from employer WHERE(location = "St Louis City");
 
 ## Part 3: Test it with SQL
+DROP TABLE job
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM skill
+WHERE job_id is not null;
